d2a82ad0d798bdefcf63163eb42b869306aa180e
Johannes Scharna <j.scharna@rialgo.de>
2021-12-17 12:51:05 +0100

    Implemented client interface methods

--- ts/erdstallclientinterface.ts
+++ ts/erdstallclientinterface.ts
@@ -3,9 +3,12 @@ import { utils, ethers } from "ethers";
 
 import { Assets, Amount } from "@polycrypt/erdstall/ledger/assets";
 import { Address } from "@polycrypt/erdstall/ledger";
-import { Session } from "@polycrypt/erdstall";
-
+import { ErdstallEvent, Session } from "@polycrypt/erdstall";
 import detectEthereumProvider from "@metamask/detect-provider";
+
+import NFT from "./nft"
+
+import * as test from "@polycrypt/erdstall/test";
 
 export type NetworkName =
 	| "Ropsten"
@@ -22,57 +25,88 @@ export const NetworkID = new Map<number, NetworkName>([
 	[1337, "localhost"],
 ]);
 
-export async function getSessionBalance(
-	session: Session ): Promise<
-	{ balance: number } | undefined
-> {
-	// if (!session) return;
-	// session
-	// 	.getOwnAccount()
-	// 	.then((account) => {
-	// 		const amount = getPrnAmount(account.values);
-	// 		console.log("Fetched Session amount: " + amount)
-	// 		return amount;
-	// 	})
-	// 	.catch((error) => console.error(error));
-	// return undefined;
-	if (!session) return undefined;
-	var account = await session.getOwnAccount();
-	const balance: number = getPrnAmount(account.values);
-	// console.log("Fetched Session amount: " + balance)
-	return { balance };
-}
+export type eventCallback = (error: string | Error) => void;
 
-export async function getMetamaskSession(): Promise<
-{ session: Session } | undefined
-> {
-    //TODO: Load parameters from config
-    const networkID = 1337 //localhost
-    const erdOperatorUrl = new URL("ws://127.0.0.1:8401/ws");
-    const res = await getAccountProvider(networkID);
-    if (!res) return;
-    const { account, web3Provider } = res;
-    //setProvider(web3Provider);
-    const address = Address.fromString(account);
-    const signer = web3Provider.getSigner();
-    const session: Session = new Session(
-        address,
-        signer,
-        erdOperatorUrl
-    );
-    // session.initialize().then(() => {
-    //     session.subscribeSelf();
-    //     session.onboard();
-	// 	console.log("Initialized new session: " + account)
-	// 	return { session };
-    //     //setSession(session);
-    // });
-	await session.initialize();
-	await session.subscribeSelf();
-	await session.onboard();
+export default class erdstallClientInterface {
+	_session: Session | undefined;
 
-	console.log("Initialized new session: " + account)
-	return { session };
+	constructor() {
+		
+	}
+
+	async init(
+		networkID: number = 1337,
+		erdOperatorUrl: URL = new URL("ws://127.0.0.1:8401/ws")): Promise<void> {
+		//TODO: Load parameters from config
+		const res = await getAccountProvider(networkID);
+		if (!res) {
+			throw new Error("Unable to get Account Provider for network ID " + networkID);
+		}
+		const { account, web3Provider } = res;
+		//setProvider(web3Provider);
+		const address = Address.fromString(account);
+		const signer = web3Provider.getSigner();
+		const session: Session = new Session(
+			address,
+			signer,
+			erdOperatorUrl
+		);
+		try {
+			await session.initialize();
+			await session.subscribeSelf();
+			await session.onboard();
+		} catch (error) {
+			if(error) {
+				throw new Error("Error initializing metamask session" + error);
+			}
+			else {
+				throw new Error("Error initializing metamask session");
+			}
+		}
+	
+		this._session = session;
+		console.log("Initialized new session: " + account);
+	}
+
+	async getNFTs(): Promise< //TODO: implement
+		{ nfts: NFT[] } | undefined
+	> {
+		if (!this._session) return undefined;
+		let nfts: NFT[] = new Array[10];
+		for( let i = 0; i < 10; ++i) {
+			const rng = test.newPrng();
+			let nft: NFT = new NFT(
+				test.newRandomAddress(rng), //token
+				test.newRandomUint64(rng), //ID
+				test.newRandomAddress(rng), //Owner
+				undefined, //Offer
+				test.newRandomMetadata(rng)); //Metadata
+			nfts[i] = nft;
+		}
+		return { nfts };
+	}
+
+	registerCallback(
+		event: ErdstallEvent,
+		callback: eventCallback
+	) {
+		if (!this._session) throw new Error("Client session uninitialized");
+		this._session.on(event, callback);
+		console.log("Added new callback: " + event);
+	}
+
+	async getPRNBalance(): Promise<
+		{ balance: number } | undefined
+	> {
+		if (!this._session) return undefined;
+		const account = await this._session.getOwnAccount();
+		const balance = getPrnAmount(account.values);
+		if(balance) {
+			return { balance };
+		} else {
+			return undefined;
+		}
+	}
 }
 
 async function getAccountProvider(
@@ -130,13 +164,13 @@ async function getAccountProvider(
 	return { account, web3Provider };
 }
 
-function getPrnAmount(assets: Assets): number {
+function getPrnAmount(assets: Assets): number | undefined {
 	// Workaround: return the first ERC20 token we can find.
 	// FIXME: add proper querying of PRN token.
 	for (const [addr, asset] of assets.values.entries())
 		if (!Address.fromString(addr).isZero() && asset instanceof Amount)
 			return Number(utils.formatEther((asset as Amount).value));
-	return 0;
+	return undefined;
 }
 
 async function initWeb3(): Promise<ethers.providers.Web3Provider> {
