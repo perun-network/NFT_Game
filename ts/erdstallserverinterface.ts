234248ae0fd1c2d93d4c431255366d124602f5ce
Johannes Scharna <j.scharna@rialgo.de>
2021-12-21 12:40:26 +0100

    Prettified comments in interfaces

--- ts/erdstallserverinterface.ts
+++ ts/erdstallserverinterface.ts
@@ -7,7 +7,7 @@ import { TxReceipt } from "@polycrypt/erdstall/api/responses";
 
 export default class erdstallServerInterface extends erdstallClientInterface {
 	
-	//Initializes _session member and subscribes and onboards session to the erdstall system, returns wallet address as string
+	// Initializes _session member and subscribes and onboards session to the erdstall system, returns wallet address as string
 	async init(
 		networkID: number = 1337,
 		erdOperatorUrl: URL = new URL("ws://127.0.0.1:8401/ws")
@@ -16,7 +16,7 @@ export default class erdstallServerInterface extends erdstallClientInterface {
 		return {account: "abcdefg"};
 	}
 
-	//Mints a new NFT and returns TxReceipt promise
+	// Mints a new NFT and returns TxReceipt promise
 	async mintNFT(
 		address: Address,
 		id: bigint = BigInt(Math.round(Math.random() * Number.MAX_SAFE_INTEGER))
@@ -27,7 +27,7 @@ export default class erdstallServerInterface extends erdstallClientInterface {
 		return{txReceipt: await this._session.mint( address, id )};
 	}
 
-	//Burns NFT and returns TxReceipt promise
+	// Burns NFT and returns TxReceipt promise
 	async burnNFT(
 		nft: NFT
 	): Promise<{txReceipt: TxReceipt}> {
@@ -45,7 +45,7 @@ export default class erdstallServerInterface extends erdstallClientInterface {
 		}
 	}
 
-	//Transfers NFT from this address to another address and returns TxReceipt
+	// Transfers NFT from this address to another address and returns TxReceipt
 	async transferTo(
 		nft: NFT,
 		to: Address
@@ -65,7 +65,7 @@ export default class erdstallServerInterface extends erdstallClientInterface {
 	}
 }
 
-//Converts NFT object to Assets object
+// Converts NFT object to Assets object
 function getAssetsFromNFT(nft: NFT): Assets {
 	return new Assets({
 		token: nft.token,
