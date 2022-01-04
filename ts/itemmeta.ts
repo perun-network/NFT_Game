01b81f1b9c9fdfd4ffcceb86727a0f134b7bc4a2
Johannes Scharna <j.scharna@rialgo.de>
2022-01-04 10:14:25 +0100

    Fixed typos

--- ts/itemmeta.ts
+++ ts/itemmeta.ts
@@ -5,88 +5,88 @@ import { NFTMetadata } from "@polycrypt/erdstall/ledger/backend";
  */
 export interface BasicMeta {
     name: string;
-    extentions : [string, string][]; // two dimensional array of extention name and value
+    extensions : [string, string][]; // two dimensional array of extension name and value
     toMetadata(): NFTMetadata;
 }
 
 
 /**
  * RawItemMeta aims to provide an upward compatible standard for item NFT Metadata.
- * A RawItemMeta object consists of an item name, and extentions. All values are in string format hence "raw". To parse values is task of a deriving class.
- * Extentions specify further attributes such as the color code or damage, image, animation, music, etc...
- * Using Extentions is optional and an item is supposed to be fully functionaly described without the use of any. Alternativly a default value fallback has to be provided.
+ * A RawItemMeta object consists of an item name, and extensions. All values are in string format hence "raw". To parse values is task of a deriving class.
+ * Extensions specify further attributes such as the color code or damage, image, animation, music, etc...
+ * Using Extensions is optional and an item is supposed to be fully functionaly described without the use of any. Alternativly a default value fallback has to be provided.
  * (TODO: To be derived from a superclass to allow achievement metadata?)
  */
 export class RawItemMeta implements BasicMeta {
 
-    public static EXTENTION_COLORCODE : string = "cc"; // example extention
+    public static EXTENTION_COLORCODE : string = "cc"; // example extension
 
     public static VERBOSE = true;
 
 
     public name : string = "noname";
-    public extentions : [string, string][] = [];
-    protected readonly extentionsMaped = new Map();
+    public extensions : [string, string][] = [];
+    protected readonly extensionsMapped = new Map();
 
 
     /**
-     * Creates a raw item metadata object from a name and an extention map
+     * Creates a raw item metadata object from a name and an extension map
      * @param name item name
-     * @param extentions extention ids with values
+     * @param extensions extension ids with values
      */
-    constructor(name: string, extentions: [string, string][]) {
+    constructor(name: string, extensions: [string, string][]) {
         this.name = name;
-        this.extentions = extentions;
+        this.extensions = extensions;
 
         // copy array content to hash map for easy and fast access
-        for (var ext of extentions) {
+        for (var ext of extensions) {
             // allow empty keys and values, disallow duplicates
-            if (this.extentionsMaped.has(ext[0])) {
+            if (this.extensionsMapped.has(ext[0])) {
                 if (RawItemMeta.VERBOSE)
                     console.log("skipping duplicate meta key: " + ext[0]);
                 continue;
             }
-            this.extentionsMaped.set(ext[0], ext[1]);
+            this.extensionsMapped.set(ext[0], ext[1]);
         }
     }
 
 
     /**
-     * Adds an extention to the meta stack
-     * @param id extention id, see IDs as string constants in this class 
-     * @param value extention value
+     * Adds an extension to the meta stack
+     * @param id extension id, see IDs as string constants in this class 
+     * @param value extension value
      */
-    public addExtention(id: string, value: string) {
+    public addExtension(id: string, value: string) {
 
-        if (this.extentionsMaped.has(id)) {
+        if (this.extensionsMapped.has(id)) {
             if (RawItemMeta.VERBOSE)
                 console.log("ignoring duplicate meta key: " + id);
             return;
         }
 
-        this.extentions.push([id, value]);
-        this.extentionsMaped.set(id, value);
+        this.extensions.push([id, value]);
+        this.extensionsMapped.set(id, value);
     }
 
 
     /**
-     * Checks wether an extention with the specified ID is contained
-     * @param id extention id
+     * Checks wether an extension with the specified ID is contained
+     * @param id extension id
      * @returns true if contained, false if not
      */
-    public hasExtention(id: string) : boolean {
-        return this.extentionsMaped.has(id);
+    public hasExtension(id: string) : boolean {
+        return this.extensionsMapped.has(id);
     }
 
 
     /**
-     * Returns the extention value if extention with id is contained in metadata
-     * @param id extention id
-     * @returns string as extention value if contained, undefined if extention not present
+     * Returns the extension value if extension with id is contained in metadata
+     * @param id extension id
+     * @returns string as extension value if contained, undefined if extension not present
      */
-    public getExtention(id: string) : string | undefined {
-        if (this.hasExtention(id))
-            return this.getExtention(id);
+    public getExtension(id: string) : string | undefined {
+        if (this.hasExtension(id))
+            return this.getExtension(id);
         return undefined;
     }
 
@@ -140,7 +140,7 @@ export class RawItemMeta implements BasicMeta {
     public static getMetaFromJSON(json: string): RawItemMeta {
         // TODO: Format checking
         BasicMeta: var metaLookup = <BasicMeta> JSON.parse(json);
-        RawItemMeta: var metaObject = new RawItemMeta(metaLookup.name, metaLookup.extentions);
+        RawItemMeta: var metaObject = new RawItemMeta(metaLookup.name, metaLookup.extensions);
         return metaObject;
     }
 }
