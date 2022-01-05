1e6f7731102cf2e8a2148f349dffbb41a2fdce48
DeltaTecs <martin.bach@stud.tu-darmstadt.de>
2022-01-05 16:20:03 +0100

    Metadata Server: Perun key format reimplemented #16

--- ts/metadata.ts
+++ ts/metadata.ts
@@ -17,9 +17,9 @@ import { Address } from "@polycrypt/erdstall/ledger";
 //import { key as nftKey, parseKey } from "./nft";
 
 import { RawItemMeta } from "./itemmeta";
+import { key } from "./nft";
 
 export const DB_PREFIX_METADATA = "md";
-export const USE_LEGACY_KEY_FORMAT = true;
 
 export const StatusNoContent = 204;
 export const StatusNotFound = 404;
@@ -57,12 +57,6 @@ export default class NFTMetaServer {
 	}
 
 	/**
-	 * TODO: Remove 
-	 */
-	async init(): Promise<void> {
-	}
-
-	/**
 	 * Creates handler for requests
 	 * @returns Router router to with registered request handlers
 	 */
@@ -77,12 +71,14 @@ export default class NFTMetaServer {
 
 	/**
 	 * Looks up Metadata for token with given address and id
+	 * @param ownerAddr address of NFT owner (aka "token")
 	 * @param tokenId 256bit integer ID of NFT
 	 * @returns metadata
 	 */
-	getMetadata(tokenId: BigInt): RawItemMeta | undefined {
+	getMetadata(ownerAddr : Address, tokenId: bigint): RawItemMeta | undefined {
+
 		try {
-			const meta = this.databaseHandler.getNFTMetadata(tokenId); // ## Redis interface to accept NFT IDs as keys
+			const meta = this.databaseHandler.getNFTMetadata(key(ownerAddr, tokenId));
 			if((meta == undefined) && this.cfg.serveDummies) {
 				return this.dummyMetadata();
 			}
@@ -100,10 +96,10 @@ export default class NFTMetaServer {
 	 * @returns dummy meta data object
 	 */
 	dummyMetadata(): RawItemMeta {
-		return new RawItemMeta("dummy", []);
+		let metadata : RawItemMeta = new RawItemMeta([]);
+		metadata.addAttribute(RawItemMeta.ATTRIBUTE_NAME, "noname");
+		return metadata;
 	}
-	/********** REST API **********/
-
 
 	/**
 	 * looks up meta data for token in request and sends it to the response
@@ -112,17 +108,19 @@ export default class NFTMetaServer {
 	 * @returns 
 	 */
 	private async getNft(req: Request, res: Response) {
-		const tokenId = BigInt(req.params.id); // parse Token identifier (assumed globaly unique) in http request
+		// params is part of the request f.e. http://yadayada.de/yomama?token=0x69696969696969420...
+		const ownerAddr : Address = Address.fromString(req.params.token); // parse Address params field in http request
+		const tokenId : bigint = BigInt(req.params.id); // parse Token identifier (assumed globaly unique) in http request
 		// assume token id's to be unique systemwide and treat them as primary key
 		// TODO: Fix uncaught exception somehow (simple try catch doesn't work for whatever reason)
-		const meta = this.getMetadata(tokenId); // lookup meta data
+		const meta : RawItemMeta | undefined = this.getMetadata(ownerAddr, tokenId); // lookup meta data
 		if (!meta) {
 			// send 404
 			res.status(StatusNotFound).send("No Metadata present.");
 			return;
 		}
 
-		res.send(meta.asJSON()); // origianly sending without conversion
+		res.send(meta.asJSON()); // originaly sending without conversion
 	}
 
 	/**
@@ -134,17 +132,12 @@ export default class NFTMetaServer {
 	private async putNft(req: Request, res: Response) {
 
 		// params is part of the request f.e. http://yadayada.de/yomama?token=0x69696969696969420...
-		const ownerAddr = Address.fromString(req.params.token); // parse Address params field in http request
-		const tokenId = BigInt(req.params.id); // parse Token identifier (assumed globaly unique) in http request
-
-		// how will the request body look like? Is only the metadata JSON? 
-		// parse request to Metadata
-		var requestBody : string = req.body;
-		var meta : RawItemMeta = RawItemMeta.getMetaFromJSON(requestBody); // assuming body has meta JSON format... (TODO: format checking?)
+		const ownerAddr : Address = Address.fromString(req.params.token); // parse Address params field in http request
+		const tokenId : bigint = BigInt(req.params.id); // parse Token identifier (assumed globaly unique) in http request
 
 		try {
-			this.databaseHandler.putNFTMetadata(req.params.token, meta.asJSON()); // directly adding requestBody maybe more efficient but this is more save
-			await this.afterMetadataSet(tokenId); // run Observers
+			this.databaseHandler.putNFTMetadata(key(ownerAddr, tokenId), req.body);
+			await this.afterMetadataSet(ownerAddr, tokenId); // run Observers
 			res.sendStatus(StatusNoContent); // send success without anything else
 		// TODO: Maybe implement this.cfg.allowUpdates handling
 		} catch (error) { // Handle NFT already being present in database
@@ -153,7 +146,7 @@ export default class NFTMetaServer {
 	}
 
 	// can be overridden in derived classes
-	protected async afterMetadataSet(_key: BigInt): Promise<void> {
+	protected async afterMetadataSet(ownerAddr : Address, tokenId: bigint): Promise<void> {
 		return;
 	}
 
@@ -164,10 +157,12 @@ export default class NFTMetaServer {
 	 */
 	private async deleteNft(req: Request, res: Response) {
 
-		const tokenId = BigInt(req.params.id); // parse Token identifier (assumed globaly unique) in http request
+		// params is part of the request f.e. http://yadayada.de/yomama?token=0x69696969696969420...
+		const ownerAddr : Address = Address.fromString(req.params.token); // parse Address params field in http request
+		const tokenId : bigint = BigInt(req.params.id); // parse Token identifier (assumed globaly unique) in http request
 
 		try {
-			await this.databaseHandler.deleteNFTMetadata(tokenId);
+			await this.databaseHandler.deleteNFTMetadata(key(ownerAddr, tokenId));
 			res.sendStatus(StatusNoContent); // (assuming) success, send nothing
 		} catch (error) {
 			if(error == ("NFT Metadata not in database for NFT: " + tokenId)) {
@@ -177,6 +172,7 @@ export default class NFTMetaServer {
 			}
 		}
 	}
+
 }
 
 export interface MetadataConfig {
